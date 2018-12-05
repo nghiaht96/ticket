ab6d07d6575971449a01abbdedafac7bdd8be206
hoangnam160396 <hoangnamnguyen.a10@gmail.com>
2018-12-05 14:26:45 +0700

    delegate

--- src/main/java/com/dxc/ticket/delegate/V1Delegate.java
+++ src/main/java/com/dxc/ticket/delegate/V1Delegate.java
@@ -1,4 +1,24 @@
 package com.dxc.ticket.delegate;
 
-public class V1Delegate {
+import com.dxc.ticket.api.V1ApiDelegate;
+import com.dxc.ticket.api.model.Ticket;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class V1Delegate implements V1ApiDelegate {
+    @Override
+    public ResponseEntity<String> deleteTicket(String idTicket) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Ticket> getTicketByIdTicket(String idTicket) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> upsertTicket(Ticket ticket) {
+        return null;
+    }
 }
